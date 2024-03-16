652bf1542edcbff3460516aca2c4d31d86273d13
LMT <2788206617@qq.com>
2024-03-16 18:26:33 +0800

    change

--- 第一次作业/MainActivity.kt
+++ 第一次作业/MainActivity.kt
@@ -1,7 +1,6 @@
-package com.example.lmt2207020310no1
+package com.example.xzc2207020314no2
 
 import android.os.Bundle
-import android.os.Message
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
@@ -10,7 +9,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.magnifier
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -23,21 +21,22 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.lmt2207020310no1.ui.theme.LMT2207020310No1Theme
+import com.example.xzc2207020314no2.ui.theme.XZC2207020314No2Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            LMT2207020310No1Theme {
+            XZC2207020314No2Theme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    LMT2207020310No1Theme {
-                        GreetingImage(message = "Happy Birthdays sam!", from = "From lmt")
+                    XZC2207020314No2Theme {
+                        GreetingImage(message = "Happy Birthday Yuuka!" , from = "From lfxy")
                     }
+
                 }
             }
         }
@@ -45,57 +44,52 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun GreetingText(message: String, from : String , modifier: Modifier = Modifier) {
-    Column(
+fun GreetingText(message: String, from:String, modifier: Modifier = Modifier) {
+    Column (
         verticalArrangement = Arrangement.Center,
         modifier = modifier
-    ) {
+    ){
         Text(
             text = message,
-            fontSize = 100.sp,
-            lineHeight = 116.sp,
+            fontSize = 50.sp,
+            lineHeight = 100.sp,
             textAlign = TextAlign.Center
         )
         Text(
             text = from,
-            fontSize = 36.sp,
+            fontSize = 50.sp,
             modifier = Modifier
                 .padding(8.dp)
                 .align(alignment = Alignment.CenterHorizontally)
         )
-
     }
 }
 
-
 @Composable
-fun GreetingImage(message: String,from: String,modifier: Modifier = Modifier) {
-    val image = painterResource(id = R.drawable.androidparty)
-    Box(Modifier) {
+fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier){
+    val image = painterResource(id = R.drawable.yuuka)
+    Box(Modifier){
         Image(
             painter = image,
             contentDescription = null,
             contentScale = ContentScale.Crop,
-            alpha = 0.5F
+            alpha = 0.7F
         )
         GreetingText(
-            message = message,
-            from = from,
+            message = message, from = from,
             modifier = Modifier
                 .fillMaxSize()
                 .padding(8.dp)
         )
     }
 }
-
-
 @Preview(
     showBackground = true,
     showSystemUi = true
 )
 @Composable
 fun GreetingPreview() {
-    LMT2207020310No1Theme {
-        GreetingImage(message = "Happy Birthdays sam!", from = "From lmt")
+    XZC2207020314No2Theme {
+        GreetingImage(message = "Happy Birthday Yuuka!" , from = "From lfxy")
     }
 }
